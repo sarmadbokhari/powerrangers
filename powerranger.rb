b2240ca2bd9eeddb6c9975440a6fc4c5a99294ff
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-03-14 17:50:46 -0500

    person, ranger, ninja classes & punch, fightscene modules

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,31 @@
+module Punch
+  def punch(person)
+    puts "Poww! to #{person}"
+    person.scream
+    person.run
+    @caffeine_level -= 1
+
+    if @strength >= 5
+      puts "#{person.name} was somersaulted into the air"
+      @caffeine_level -= 2
+    end
+  end
+end
+
+module FightScene
+  def fight(ranger1, ranger2, ninja1, ninja2, person1, person2)
+    ranger1.punch(ninja1)
+    ninja2.punch(ranger2)
+    ninja1.cause_mayhem(person1)
+    person1.scream
+    person2.run
+    ranger2.drink_coffee(5000)
+    ranger2.use_megazord(ninja1)
+  end
+end
+
 class Person
+  include FightScene
 attr_reader :name, :cups
 attr_accessor :caffeine_level
 
@@ -16,29 +43,21 @@ attr_accessor :caffeine_level
   end
 
   def drink_coffee(cups)
+    puts "I just drank #{cups} cups of coffee"
     @caffeine_level += cups
   end
 end
 
 class PowerRanger < Person
+  include Punch
+  include FightScene
 attr_reader :color
 attr_accessor :strength
-  def initialize(caffeine_level=0, strength, color)
-    super(caffeine_level)
-    @strength = strength
+
+  def initialize(name, color, caffeine_level=10, strength=10)
+    super(name, caffeine_level)
+    @strength = caffeine_level
     @color = color
-  end
-
-  def punch(person)
-    puts "Poww! to #{person}"
-    person.scream
-    person.run
-    @caffeine_level -= 1
-
-    if @strength >= 5
-      puts "#{person} was somersaulted into the air"
-      @caffeine_level -= 2
-    end
   end
 
   def use_megazord(person)
@@ -57,5 +76,24 @@ attr_accessor :strength
   end
 end
 
-class EvilNinja
+class EvilNinja < Person
+include Punch
+include FightScene
+  def initialize(name, caffeine_level=10, strength, evilness)
+    super(name, caffeine_level)
+    @strength = caffeine_level
+    @evilness = evilness
+  end
+
+  def cause_mayhem(person)
+    person.caffeine_level = 0
+    puts "#{person.name} caffeine level reduced to smitherines"
+  end
 end
+
+john = Person.new("John")
+jacob = Person.new("Jacob")
+kate = PowerRanger.new("Kate", "white")
+susy = PowerRanger.new("Susy", "pink")
+martin = EvilNinja.new("Martin", 100, 100)
+smith = EvilNinja.new("Smith", 100, 100)
